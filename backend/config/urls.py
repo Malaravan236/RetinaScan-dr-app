389b54309e6744959bade77e7d9a05ec2dc7059b
malaravan236@gmail.com <malaravan236@gmail.com>
2026-09-18 18:42:23 +0530

    Fix media image serving

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("api/", include("diagnosis.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
